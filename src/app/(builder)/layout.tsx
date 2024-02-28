0e1956f01cba2155a112682ad62b9a144d303f72
Piotr Berlowski <piotr.berlowski@gmail.com>
2024-02-28 01:01:19 +0000

    Moving to vercel banner

--- src/app/(builder)/layout.tsx
+++ src/app/(builder)/layout.tsx
@@ -16,12 +16,28 @@ export const metadata: Metadata = {
   description: 'A minimal search interface and List Builder backed by MasterUnitList.info',
 }
 
-function Header() {
-  return (
-    <div className='flex flex-row h-12 min-h-full border border-black border-black dark:border-white border-solid rounded-md items-center align-bottom print:hidden'>
-      <span className='basis-full inline-block sm:text-xl md:text-2xl text-center'>
+function VercelMoveBanner() {
+  if (process.env.HOSTING?.toLowerCase() == "vercel") {
+    return (
+      <div className='w-full sm:text-xl md:text-2xl text-center'>
         <Link href="/">List Builder for AS ({publicRuntimeConfig?.version})</Link> powered by <Link href="http://masterunitlist.info" target="_blank">Master Unit List API</Link>
-      </span>
+      </div>
+    )
+  }
+  return (
+    <div className='w-full text-center bg-red-700 text-white rounded-md'>
+      <div className='w-full sm:text-xl md:text-2xl'>List Builder is moving to a new home <a href="https://as-builder.vercel.app">https://as-builder.vercel.app</a></div>
+      <div className='w-full'>Please migrate your stored lists and update your bookmarks.</div>
+    </div>
+  )
+}
+
+
+function Header() {
+
+  return (
+    <div className='flex flex-wrap min-h-12 min-h-full border border-black border-black dark:border-white border-solid rounded-md items-center align-bottom print:hidden'>
+      <VercelMoveBanner />
     </div>
   )
 }
@@ -30,7 +46,7 @@ function FloatingFooter() {
   return (
     <div className='sticky bottom-0 mt-2 items-justified text-center bg-inherit border-t border-t-solid border-t-1 border-t-black dark:border-t-white text-xs print:hidden'>
       <div className='w-full items-right relative'>
-        <ScrollToTop className="absolute -top-8"/>
+        <ScrollToTop className="absolute -top-8" />
       </div>
       <div className='grid grid-cols-3 w-full'>
         <span>Data and search API courtesy of <a href="http://www.masterunitlist.info">Master Unit List.</a></span>
